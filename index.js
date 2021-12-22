3116171601e41f72f681033c089d3ad09f15f674
Shakib88 <shakibeeeuap28@gmail.com>
2021-12-22 14:09:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Bimillah"); //Bimillah
+console.log("Alhamdulillahi Rabbil Alamin"); //Alhamdulillahi Rabbil Alamin
+console.log(1); //1
+console.log(1.33); //1.33
+
+//string+number=string
+console.log('My favourite Number is: ' + 1); //My favourite Number is: 1
+
+
+
